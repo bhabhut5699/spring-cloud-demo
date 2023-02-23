1e7eed578c9eed57ee5cc86fc9856c17a345d562
sohil <Sohil.techexpert@gmail.com>
2023-02-23 13:30:48 +0530

    first commit

---
+++ sample-service/src/main/java/com/spring/cloud/example/SampleServiceApplication.java
@@ -0,0 +1,14 @@
+package com.spring.cloud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SampleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServiceApplication.class, args);
+    }
+}
---
+++ sample-service/src/main/java/com/spring/cloud/example/controllers/ConfigurationApi.java
@@ -0,0 +1,25 @@
+package com.spring.cloud.example.controllers;
+
+import com.spring.cloud.example.model.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RefreshScope
+@RestController
+@RequestMapping("/api")
+public class ConfigurationApi {
+
+    @Autowired
+    private Request request;
+
+    @GetMapping("/configuration")
+    public ResponseEntity getConfiguration() {
+        return ok().body(request);
+    }
+}
